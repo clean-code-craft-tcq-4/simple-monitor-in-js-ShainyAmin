8ab2f48128eefb407aef2629f4cea203d5e53481
ShainyAmin <38464281+ShainyAmin@users.noreply.github.com>
2022-07-17 13:23:09 +0530

    Add files via upload

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,44 +1,82 @@
 const {expect} = require('chai');
+const config = require('./bmsConfiguration.json');
 
-function checkBatteryTemperature(temperature) {
-    console.log("checkBatteryTemperature: ",temperature)
-    if (temperature < 0 || temperature > 45) {
-        console.log('Temperature is out of range!', temperature);
-        return false;
-    }
-    
-        return true;
-    
-}
-function checkBatterySOC(soc) {
-    console.log("checkBatterySOC: ",soc)
-    if (soc < 20 || soc > 80) {
-        console.log('State of Charge is out of range!')
-        return false;
-    } 
-   
-        return true;
-    
+function calculateWarningTolerance(maxLimit) {
+   return maxLimit * config.WARNING_TOLERANCE_PERCENT; 
 }
 
-function checkBatteryChargeRate(charge_rate) {
-    console.log("checkBatteryChargeRate: ",charge_rate)
-    if (charge_rate<=0 || charge_rate > 0.8) {
-        console.log('Charge rate is out of range!');
-        return false;
+let language = '';
+
+const batteryIsOk = (temperature, soc, chargeRate, lang) => {
+  language = lang;
+  let langIsEligible = languageIsEligible(lang);
+  if(langIsEligible){
+    const tempResult = temperatureIsOK(temperature);
+    const socResult = socIsOK(soc);
+    const chargeResult = chargeRateIsOK(chargeRate);
+    return tempResult && socResult && chargeResult;
+  }
+  else
+    return false;
+ };
+
+function languageIsEligible(lang) {
+    return config.LANGUAGES_SUPPORTED.includes(lang.toUpperCase())
+}
+
+const temperatureIsOK = (temperature) => {
+    return checkBatteryState(temperature, config.MIN_TEMP, config.MAX_TEMP, 'TEMP');
+  };
+  
+  const socIsOK = (soc) => {
+    return checkBatteryState(soc, config.MIN_SOC, config.MAX_SOC, 'SOC');
+  };
+  
+  const chargeRateIsOK = (chargeRate) => {
+    return checkBatteryState(chargeRate, config.MIN_CHARGE_RATE, config.MAX_CHARGE_RATE, 'CHARGE_OUT');
+  };
+
+  const checkBatteryState = (batteryFactorValue, minLimit, maxLimit, bmsFactor) => {
+    checkWarningLevel(minLimit, maxLimit, batteryFactorValue, bmsFactor);
+    if (batteryFactorValue >= maxLimit || batteryFactorValue <= minLimit) {
+          printStatement(bmsFactor,  "OUT_OF_RANGE");
+      return false;
     }
     return true;
-}
+  };
 
-expect(checkBatteryTemperature(25)).to.be.true;
-expect(checkBatterySOC(70)).to.be.true;
-expect( checkBatteryChargeRate(0.7)).to.be.true;
+  const printStatement = (statement, msg) => {
+    var lang = language.toUpperCase();
+    var msgCode = config[lang][statement] + " "+ config[lang][msg];
+    console.log(msgCode);
+  };
+  
+  const checkWarningLevel = (lowerLimit, upperLimit, value, statement) => {
+    const WarningLimit = calculateWarningTolerance(upperLimit);
+  
+    if (lowerLimit > value) {
+        printStatement(statement, "LOW_BREACH" );
+      
+    }
+    if (lowerLimit <= value && lowerLimit + WarningLimit >= value) {
+        printStatement(statement, "LOW_WARNING" );
+     
+    }
+    if (
+      lowerLimit + WarningLimit <= value &&
+      upperLimit - WarningLimit >= value
+    ) {
+        printStatement(statement, "NORMAL" );
+      
+    }
+    if (upperLimit - WarningLimit <= value && value >= upperLimit) {
+        printStatement(statement, "HIGH_WARNING" );
+     
+    }
+    if (upperLimit > value) {
+        printStatement(statement, "HIGH_BREACH" );
+    
+    }
+  };
 
-expect(checkBatteryTemperature(50)).to.be.false;
-expect(checkBatterySOC(85)).to.be.false;
-expect( checkBatteryChargeRate(0)).to.be.false;
-
-
-
-// expect(batteryIsOk(25, 70, 0.7)).to.be.true;
-// expect(batteryIsOk(50, 85, 0)).to.be.false;
+  module.exports = { batteryIsOk}
